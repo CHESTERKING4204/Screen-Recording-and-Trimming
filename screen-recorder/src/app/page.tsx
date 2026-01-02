2f5eea8f9f7cbea3208fcab006468d364514143a
SHIVANSH GARG <114911683+CHESTERKING4204@users.noreply.github.com>
2026-01-02 16:14:10 +0530

    Update page.tsx

--- screen-recorder/src/app/page.tsx
+++ screen-recorder/src/app/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
 
 'use client';
+export const dynamic = "force-dynamic";
 
 import { useState } from 'react';
 import ScreenRecorder from '@/components/ScreenRecorder';
@@ -64,4 +64,5 @@ export default function Home() {
     </main>
   );
 }
+
 
